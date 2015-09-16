cde1ebe627e576b536dc92461aeb45f32c9d8cfb
Kevin Lin <kevinlin505@hotmail.com>
2015-09-16 16:44:46 -0400

    Classy methods done

--- objc-classy-methods/FISAppDelegate.m
+++ objc-classy-methods/FISAppDelegate.m
@@ -11,12 +11,39 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    NSString *katherine = @"Katherine";
+    NSString *katherineHepburn = [katherine stringByAppendingString:@" Hepburn"];
+    NSLog(@"%@", katherineHepburn);
     
-    /**
-     
-     *  Write your code here.
-     
-     */
+    NSString *james = @"James";
+    NSString *jamesStewart = [[NSString alloc] initWithFormat:@"%@ %@", james, @"Stewart"];
+    NSLog(@"%@", jamesStewart);
+    
+    NSString *queen = @"Queen";
+    NSString *queenElizabethII = [NSString stringWithFormat:@"%@ %@ %@", queen, @"Elizabeth", @"II"];
+    NSLog(@"%@", queenElizabethII);
+    
+    
+    NSArray *classyThings = @[@"monocle", @"top hat", @"martini glass"];
+    NSLog(@"%@", classyThings);
+    
+    NSArray *classyPeople = [[NSArray alloc] initWithObjects:katherineHepburn, jamesStewart, queenElizabethII, nil];
+    NSLog(@"%@", classyPeople);
+    
+    NSArray *classyDrinks = [NSArray arrayWithObjects:@"Old Fashioned", @"Churchill Martini", @"Prosecco", nil];
+    NSLog(@"%@", classyDrinks);
+    
+    NSDictionary *classyByLiteral = @{@"classy things"  :   classyThings,
+                                      @"classy people"  :   classyPeople,
+                                      @"classy drinks"  :   classyDrinks};
+    NSLog(@"%@", classyByLiteral);
+    
+    NSDictionary *classyByInit = [[NSDictionary alloc] initWithObjectsAndKeys:classyThings, @"classy thins", classyPeople, @"classy people", classyDrinks, @"classy drinks", nil];
+    NSLog(@"%@", classyByInit);
+    
+    NSDictionary *classyByClass = [NSDictionary dictionaryWithObjects:@[classyThings, classyPeople, classyDrinks] forKeys:@[@"classy things", @"classy people", @"classy drinks"]];
+    NSLog(@"%@", classyByClass);
     
     return YES;
 }
