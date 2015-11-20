8b509842534b779be04dce34a46e91099c97b923
richardgmartin <richardgeorgemartin@gmail.com>
2015-11-20 17:00:51 -0500

    completed classy method exercises

--- objc-classy-methods/FISAppDelegate.m
+++ objc-classy-methods/FISAppDelegate.m
@@ -10,13 +10,84 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    // 1. code along I -- nsstring
     
-    /**
-     
-     *  Write your code here.
-     
-     */
+    
+    // a. use an instance method
+    
+    NSString *katherine = @"Katherine";
+    
+    NSString *katherineHepburn = [katherine stringByAppendingString:@"Hepburn"];
+    
+    NSLog(@"%@", katherineHepburn);
+    
+    // b. use an init ... method
+    
+    NSString *james = @"James";
+    
+    NSString *jamesStewart = [[NSString alloc] initWithFormat:@"%@ %@", james, @"Stewart"];
+    
+    NSLog(@"%@", jamesStewart);
+    
+    // c. class method
+    
+    NSString *queen = @"Queen";
+    
+    NSString *queenElizabethII = [NSString stringWithFormat:@"%@ %@ %@",queen, @"Elizabeth",@"II"];
+    
+    NSLog(@"%@", queenElizabethII);
+    
+    // 2. code along II -- nsarray
+    
+    
+    // a. use the literal array
+    
+    NSArray *classyThings = @[@"monocle",@"top hat",@"martini glass"];
+    
+    NSLog(@"%@", classyThings);
+    
+    // b. use an initializer method
+    
+    NSArray *classyPeople = [[NSArray alloc] initWithObjects:@"Katherine Hepburn",@"James Stewart",@"Queen Elizabeth II", nil];
+    
+    NSLog(@"%@", classyPeople);
+    
+    // c. use a class method
+    
+    NSArray *classyDrinks = [NSArray arrayWithObjects:@"Old Fashioned",@"Churchill Martini", @"Prosecco", nil];
+    
+    NSLog(@"%@", classyDrinks);
+    
+    // 3. code along III - nsdictionary
+    
+    // a. use a dictionary literal
+    
+    NSDictionary *classyByLiteral = @{@"classy things"  :@[@"monocle"   ,
+                                                           @"top hat"  ,
+                                                           @"martini glass"],
+                                      @"classy people"  :@[@"Katherine Hepburn"   ,
+                                                           @"James Stewart"  ,
+                                                           @"Queen Elizabeth II"],
+                                      @"classy drinks"  :@[@"Old Fashioned"   ,
+                                                           @"Churhill Martini"  ,
+                                                           @"Prosecco"]
+                                      };
+    
+    NSLog(@"%@", classyByLiteral);
+    
+    // b. use an initializer method
+    
+    NSDictionary *classyByInit = [[NSDictionary alloc] initWithObjectsAndKeys:classyThings,@"classy things", classyPeople, @"classy people", classyDrinks, @"classy drinks", nil];
+    
+    NSLog(@"%@", classyByInit);
+    
+    // c. use a class method
+    
+    NSDictionary *classByClass = [NSDictionary dictionaryWithObjects:@[classyThings, classyPeople, classyDrinks]
+                                                             forKeys:@[@"classy things", @"classy people", @"classy drinks"]];
+    
+    NSLog(@"%@", classByClass);
     
     return YES;
 }
